53cb7489e3dba9a84fcbfc652594f834481c0b85
miroNever <123352959+miroNever@users.noreply.github.com>
2023-04-19 11:39:39 +0500

    Add files via upload

--- pr14_Poleev/Program.cs
+++ pr14_Poleev/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace pr14_Poleev
@@ -26,8 +27,7 @@ namespace pr14_Poleev
         static void Zadanie1()
         {
             Stack<int> number = new Stack<int>();
-            Console.Write
-                ("Введите число n: ");
+            Console.Write("Введите число n: ");
             int n = CheckNum();
             for (int i = 1; i <= n; i++)
             {
@@ -43,11 +43,97 @@ namespace pr14_Poleev
                     break;
             }
             Console.WriteLine($"\nНовая размерность стека = {number.Count}");
-            Console.ReadKey();
+        }
+        static void Zadanie2()
+        {
+            Console.Write("Введите математической выражение: ");
+            string expression = Console.ReadLine();
+            StreamWriter sw = File.AppendText("t.txt");
+            sw.WriteLine(expression);
+            sw.Close();
+            int openingBracketCount = expression.Count(c => c == '(');
+            int closingBracketCount = expression.Count(c => c == ')');
+            int bracketBalance = openingBracketCount - closingBracketCount;
+            if (bracketBalance == 0)
+            {
+                Console.WriteLine("Скобки сбалансорованны");
+            }
+            else if (bracketBalance < 0)
+            {
+                Console.WriteLine($"Недостаточно {Math.Abs(bracketBalance)} открывающих скобок");
+            }
+            else
+            {
+                Console.WriteLine($"Недостаточно {bracketBalance}  закрывающих скобок");
+            }
+
+            if (bracketBalance > 0)
+            {
+                expression += new string(')', bracketBalance);
+                Console.WriteLine($"Для скобки в позиции {bracketBalance} не хватало скобки. Добавление закрывающих скобок: {expression}");
+            }
+            else if (bracketBalance < 0)
+            {
+                expression = expression.Remove(expression.Length + bracketBalance, Math.Abs(bracketBalance));
+                Console.WriteLine($"Удалено {Math.Abs(bracketBalance)} лишних закрывающих скобок: {expression}");
+            }
+            int lastOpeningBrackert = expression.LastIndexOf('(');
+            if (lastOpeningBrackert != -1)
+            { 
+                bool operatore = false;
+                for (int i = lastOpeningBrackert + 1; i < expression.Length; i++)
+                {
+                    if (Operator(expression[i]))
+                    {
+                        operatore = true;
+                        break;
+                    }
+                }
+                if (!operatore)
+                {
+                    expression = expression.Remove(lastOpeningBrackert, 1);
+                    Console.WriteLine($"Удалена последняя открывающаяя скобка: {expression}");
+                }
+            }
+            if (expression.StartsWith(")"))
+            {
+                expression = expression.Remove(0, 1);
+                Console.WriteLine($"Удалени первая закрывающаяя скобка: {expression}"); 
+            }
+            File.WriteAllText("t1.txt", expression);
+            Console.WriteLine($"Выражение записано в файл t1.txt: {expression}");
+        }
+        static bool Operator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
         }
         static void Main(string[] args)
         {
-            Zadanie1();
+            Console.WriteLine("Какое задание хотите сдлеать (1, 2)");
+            string zad = Console.ReadLine();
+            while (true)
+            {
+                if (zad == "1" || zad == "2")
+                {
+                    if (zad == "1")
+                    {
+                        Zadanie1();
+                        Console.WriteLine("Какое задание хотите сдлеать (1, 2)");
+                        zad = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Zadanie2();
+                        Console.WriteLine("Какое задание хотите сдлеать (1, 2)");
+                        zad = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Выберете 1 или 2");
+                    zad = Console.ReadLine();
+                }
+            }
         }
     }
 }
